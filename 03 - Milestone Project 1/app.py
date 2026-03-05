479dd3866f8b1262bacb33d08308f7fb91518fbb
devparekh24 <devparekh2424@gmail.com>
2026-03-05 17:32:53 +0530

    Refactor movie management functions for improved readability and consistency

--- 03 - Milestone Project 1/app.py
+++ 03 - Milestone Project 1/app.py
@@ -1,24 +1,24 @@
 movies = []
 
+
 def add_movie():
     title = input("Enter movie title:")
     director = input("Enter movie director:")
     year = input("Enter movie release year:")
 
-    movies.append({
-        "title":title,
-        "director":director,
-        "year":year
-    })
+    movies.append({"title": title, "director": director, "year": year})
+
 
 def show_movie():
     for m in movies:
         print_movie(m)
 
+
 def print_movie(movie):
-    print(f"Title:{movie["title"]}")
-    print(f"Director:{movie["director"]}")
-    print(f"Year:{movie["year"]}")
+    print(f"Title:{movie['title']}")
+    print(f"Director:{movie['director']}")
+    print(f"Year:{movie['year']}")
+
 
 def find_movie():
     movie_title = input("Enter movie title:")
@@ -26,27 +26,29 @@ def find_movie():
         if m["title"] == movie_title:
             print_movie(m)
 
+
 menu_selection_option = """
 To Add Movie Enter  : 'a'
 To Show Movie Enter : 's'
 To Find Movie Enter : 'f'
 To Exit From Movie Menu Enter : 'q'
 """
-user_optns={
-    "a": add_movie, # Do not call function here, just point it
+user_optns = {
+    "a": add_movie,  # Do not call function here, just point it
     "s": show_movie,
-    "f": find_movie
+    "f": find_movie,
 }
+
 
 def menu():
     selection = input(menu_selection_option)
-    while selection != 'q':
+    while selection != "q":
         # 1st method
-        if selection =='a':
+        if selection == "a":
             add_movie()
-        elif selection =='s':
+        elif selection == "s":
             show_movie()
-        elif selection =='f':
+        elif selection == "f":
             find_movie()
 
         # # 2nd method
@@ -54,8 +56,9 @@ def menu():
         #     selected_fun = user_optns[selection]
         #     selected_fun()
         else:
-            print('Unknown command, Please try again')
-            
+            print("Unknown command, Please try again")
+
         selection = input(menu_selection_option)
+
 
 menu()
